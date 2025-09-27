53b76952d188f2eb524c18a019ed640fd0e9cd08
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2025-09-27 18:05:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,12 +37,16 @@ cat > /data/data/com.termux/files/usr/bin/cursor << 'EOF'
 
 # Kill open X11 processes
 kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android
 
 # Enable PulseAudio over Network
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
+virgl_test_server_android &
 
 # Prepare termux-x11 session
 export XDG_RUNTIME_DIR=${TMPDIR}
+export PULSE_SERVER=tcp:127.0.0.1:4713
 export DISPLAY=:0
 termux-x11 :0 -dpi 240 -listen tcp >/dev/null &
 
@@ -54,7 +58,7 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
 # Login in PRoot Environment
-proot-distro login ubuntu --user root --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && sudo service dbus start && su - droidmaster -c "dbus-daemon --session --fork; env DISPLAY=:0 startxfce4"'
+proot-distro login ubuntu --user root --shared-tmp --no-sysvipc -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && sudo service dbus start && su - droidmaster -c "dbus-daemon --session --fork; env DISPLAY=:0 startxfce4"'
 
 exit 0
 EOF
